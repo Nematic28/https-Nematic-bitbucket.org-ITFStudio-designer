b9e22ab49425b2c5452eb6b1bf7ba2c52ac66c46
Vovkt <1364>
2015-04-04 21:29:59 +0500

    add jquery

---
+++ static/js/designer_form.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    var $left_button = $('.nav-left');
+    var $right_button = $('.nav-right');
+
+    var $rotations = $('.main-view');
+
+    var count = $rotations.length;
+
+});
