00c41e4381a4151dd9159be41e7f91e95494c2ec
Vovkt <1364>
2015-04-04 17:21:33 +0500

    ACAR-35: Добавил вывод основных картинок

---
+++ designer/form/designer.py
@@ -0,0 +1,86 @@
+from designer.models import Catalog
+
+
+class DesignerForm:
+    request = None
+
+    options = []
+    images = []
+
+    def __init__(self, request):
+        self.request = request
+
+    def get_checked_items_id(self):
+        result = []
+        for key, item in self.request.POST.lists():
+            if 'designer' in key:
+                result += item
+        return list(map(int, result))
+
+    def load(self):
+        root = list(Catalog.objects.child(None).published().ordered().all())
+        self.options = self.__load_options__(root)
+        self.images = self.__load_images__(self.options)
+        print(self.images)
+
+    def get_options(self):
+        return self.options
+
+    def get_images(self):
+        return self.images
+
+    def __load_images__(self, data):
+        items = self.__get_checked_items__(data)
+        view = {}
+        for item in items:
+            for image in item.images():
+                if image.number in view:
+                    view[image.number].append(image)
+                else:
+                    view[image.number] = [image]
+        return view
+
+    def __get_checked_items__(self, data):
+        checked_items_id = self.get_checked_items_id()
+        result = []
+        for item in data:
+            if item.id in checked_items_id:
+                result.append(item)
+
+        return result
+
+    def __load_options__(self, data):
+        if not data:
+            return []
+
+        result = []
+        child = []
+        for item in data:
+            result.append(item)
+
+            if item.type.is_label():
+                result += self.__load_options__(item.child())
+            else:
+                if self.__need_to_load__(item):
+                    child += item.child()
+        result += self.__load_options__(child)
+        return result
+
+    def __choice_from_form__(self, item):
+        request = self.request
+        list_of_elements = request.POST.getlist(item.input_name())
+        if list_of_elements:
+            if str(item.id) in list_of_elements:
+                return True
+        return False
+
+    def __need_to_load__(self, item):
+
+        if self.request.POST.getlist(item.input_name()):
+            return self.__choice_from_form__(item)
+        elif item.type.is_label():
+            return True
+        elif item.default:
+            return True
+
+        return False
--- designer/views.py
+++ designer/views.py
@@ -1,68 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from designer.models import Catalog, Image
-from designer.models import FieldType
-
-
-class DesignerForm:
-    request = None
-
-    def __init__(self, request):
-        self.request = request
-
-    def load(self):
-        root = list(Catalog.objects.child(None).published().ordered().all())
-        return self.__loader__(root)
-
-    def __choice_from_form__(self, item):
-        request = self.request
-        list_of_elements = request.POST.getlist(item.input_name())
-        if list_of_elements:
-            if str(item.id) in list_of_elements:
-                return True
-        return False
-
-    def __need_to_load__(self, item):
-
-        if self.request.POST.getlist(item.input_name()):
-            return self.__choice_from_form__(item)
-        elif item.type.is_label():
-            return True
-        elif item.default:
-            return True
-
-        return False
-
-    def __loader__(self, data):
-        if not data:
-            return []
-
-        result = []
-        child = []
-        for item in data:
-            result.append(item)
-
-            if item.type.is_label():
-                result += self.__loader__(item.child())
-            else:
-                if self.__need_to_load__(item):
-                    child += item.child()
-        result += self.__loader__(child)
-        return result
-
-    def get_checked_items(self):
-        result = []
-        for key, item in self.request.POST.lists():
-            if 'designer' in key:
-                result += item
-        return list(map(int, result))
+from designer.form.designer import DesignerForm
 
 
 def catalog(request):
     form = DesignerForm(request)
-    options = form.load()
+    form.load()
 
     return render_to_response('designer/base.html', {
-        "options": options,
-        "checked_items": form.get_checked_items(),
+        "options": form.get_options(),
+        "images": form.get_images(),
+        "checked_items": form.get_checked_items_id(),
         }, context_instance=RequestContext(request))
