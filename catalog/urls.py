83722f9a351c5cdb6e7515dbb1ac7c681ed25953
Vovkt <1364>
2015-04-12 16:05:47 +0500

    ACAR-51: add new action

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'catalog.views.home', name='home'),
@@ -8,5 +7,7 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        # url(r'^designer/$', 'designer.views.index'),
                        # url(r'^designer/(\d+)', 'designer.views.index'),
+                       url(r'^designer/view/(\d+)$', 'designer.views.catalog'),
                        url(r'^designer/view/$', 'designer.views.catalog'),
                        )
+
