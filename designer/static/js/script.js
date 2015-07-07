e49a6e5d9a107670ae88e12eac8ff905a096f894
Быковский Михаил <m.a.bykovsky@itfstudio.ru>
2015-07-08 03:05:34 +0600

    ACAR-59 improve css/js

--- designer/static/js/script.js
+++ designer/static/js/script.js
@@ -2,6 +2,27 @@
  * Created by bykovsky on 07.07.2015.
  */
 $(function () {
+    var $product_type = $('.type-of-product-content');
+    $product_type.each(function(){
+        var $half_height = ($(this).children('img').height() / 2).toString() ;
+        $(this).children('img').css({
+            'clip' : 'rect(auto auto '+$half_height+'px auto)',
+            top: $half_height+'px'
+        });
+        $(this).children('img').hover(function(){
+            $(this).css({
+                'clip' : 'rect('+$half_height+'px auto auto auto)',
+                top: '-'+$half_height+'px'
+            });
+        },function(){
+            $(this).css({
+                'clip' : 'rect(auto auto '+$half_height+'px auto)',
+                top: $half_height+'px'
+            });
+        });
+    });
+});
+$(function () {
     var $product = $('.type-of-product');
     var $count = $product.length;
     $product.parent().removeClass().addClass(function(){
@@ -20,4 +41,4 @@ $(function () {
     });
 
     console.log($count)
-})
+});
